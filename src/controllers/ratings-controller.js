2c01b015be49d90ca5122d1c75284009a5967450
Isaac Pontes <isaaclfp@outlook.com>
2023-02-10 21:54:21 -0300

    fix: fix validation logic

--- src/controllers/ratings-controller.js
+++ src/controllers/ratings-controller.js
@@ -3,10 +3,11 @@ const User = require("../models/User")
 module.exports = {
   rate: async function (req, res) {
     try {
-      const { _id, rating } = req.body
+      const _id = req.body._id
+      const rating = +req.body.rating
       const currentUser = req.user
 
-      if (typeof rating !== "number" || (rating < 0 && rating > 10)) {
+      if (Number.isNaN(rating) || rating < 0 || rating > 10) {
         return res.status(400).json({ error: "rating must be a number between 0 and 10" })
       }
 
