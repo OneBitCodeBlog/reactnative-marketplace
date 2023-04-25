ba406e255039b69835a3f7ed34b7bfbeb0174eb2
Isaac Pontes <isaaclfp@outlook.com>
2023-04-25 20:21:54 -0300

    feat: populate product data on get conversations endpoint

--- src/services/get-user-conversations.js
+++ src/services/get-user-conversations.js
@@ -1,4 +1,5 @@
 const Conversation = require("../models/Conversation")
+const Product = require("../models/Product")
 const User = require("../models/User")
 
 module.exports = async function getUserConversations(userId) {
@@ -9,11 +10,13 @@ module.exports = async function getUserConversations(userId) {
       $project: {
         seller: 1,
         buyer: 1,
+        product: 1,
         messages: { $slice: ['$messages', 0, 10] },
         updatedAt: 1
       }
     },
   ])
   await User.populate(conversations, { path: "seller buyer", select: 'name email phone' })
+  await Product.populate(conversations, { path: "product", select: "name price images createdAt"})
   return conversations
 }
