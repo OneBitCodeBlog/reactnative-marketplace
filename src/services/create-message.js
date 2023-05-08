b96fe99b87c24a020da75d46d743079506d57468
Isaac Pontes <isaaclfp@outlook.com>
2023-05-08 16:36:49 -0300

    fix: change updatedAt prop when saving new messages

--- src/services/create-message.js
+++ src/services/create-message.js
@@ -3,14 +3,16 @@ const Conversation = require("../models/Conversation")
 
 module.exports = async function createMessage(conversationId, { sender, receiver, content }) {
   const conversation = await Conversation.findById(conversationId)
+  const now = new Date()
   const newMessage = {
     _id: new mongoose.Types.ObjectId(),
     content,
     sender,
     receiver,
-    createdAt: new Date()
+    createdAt: now
   }
   conversation.messages.unshift(newMessage)
+  conversation.updatedAt = now
   await conversation.save()
   return newMessage
 }
